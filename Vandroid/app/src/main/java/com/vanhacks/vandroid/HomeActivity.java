57b5a0a2fe3d24c83661eefadc004befe956537e
Daniel Tsang <danieltsang94@gmail.com>
2016-03-05 22:53:04 -0800

    Take away broken code

--- Vandroid/app/src/main/java/com/vanhacks/vandroid/HomeActivity.java
+++ Vandroid/app/src/main/java/com/vanhacks/vandroid/HomeActivity.java
@@ -51,8 +51,6 @@ public class HomeActivity extends AppCompatActivity implements OnFragmentChangeL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_container);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {

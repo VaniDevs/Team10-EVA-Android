52144371ace86d35559ac2fc04e3c54e6714e065
Madeleine Chercover <madeleinechercover@172-18-137-211.ip.hootsuitemedia.com>
2016-03-05 16:17:23 -0800

    Fixed fragment transitions

--- Vandroid/app/src/main/java/com/vanhacks/vandroid/HomeActivity.java
+++ Vandroid/app/src/main/java/com/vanhacks/vandroid/HomeActivity.java
@@ -1,11 +1,10 @@
 package com.vanhacks.vandroid;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -22,6 +21,7 @@ public class HomeActivity extends AppCompatActivity implements OnFragmentChangeL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_container);
 //        setContentView(R.layout.activity_home);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -62,37 +62,16 @@ public class HomeActivity extends AppCompatActivity implements OnFragmentChangeL
 
         getFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, PersonalInfoFragment.newInstance())
+                .replace(R.id.fragment_container, PersonalInfoFragment.newInstance())
                 .commit();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void launchFragment(Fragment fragment) {
         getFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, fragment)
+                .replace(R.id.fragment_container, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .commit();
     }
 }
--- Vandroid/app/src/main/java/com/vanhacks/vandroid/PersonalInfoFragment.java
+++ Vandroid/app/src/main/java/com/vanhacks/vandroid/PersonalInfoFragment.java
@@ -1,9 +1,6 @@
 package com.vanhacks.vandroid;
 
 import android.app.Activity;
-import android.app.FragmentTransaction;
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
@@ -11,29 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.app.Fragment;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PersonalInfoFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PersonalInfoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PersonalInfoFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
     private OnFragmentChangeListener mOnFragmentChangeListener;
 
     public PersonalInfoFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     */
-    // TODO: Rename and change types and number of parameters
     public static PersonalInfoFragment newInstance() {
         PersonalInfoFragment fragment = new PersonalInfoFragment();
         Bundle args = new Bundle();
@@ -55,8 +37,6 @@ public class PersonalInfoFragment extends Fragment {
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
                 mOnFragmentChangeListener.launchFragment(PartnerInfoFragment.newInstance());
             }
         });
@@ -70,46 +50,10 @@ public class PersonalInfoFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_personal_info, container, false);
 
         return view;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
